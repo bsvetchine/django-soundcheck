2211a56a40230744179809078d40d7614c680696
Bertrand Svetchine <bertrand.svetchine@gmail.com>
2016-04-03 17:26:21 +0200

    zendesk - let's parameter organization.

--- soundcheck/zendesk/settings.py
+++ soundcheck/zendesk/settings.py
@@ -5,6 +5,7 @@ import requests
 
 # User configurable settings
 
+ZENDESK_ORGANIZATION = getattr(settings, "ZENDESK_ORGANIZATION", None)
 ZENDESK_LOGIN = getattr(settings, "ZENDESK_LOGIN", None)
 ZENDESK_PASSWORD = getattr(settings, "ZENDESK_PASSWORD", None)
 ZENDESK_TICKET_STATUSES_FILTER = getattr(
@@ -14,15 +15,16 @@ ZENDESK_TICKET_STATUSES_FILTER = getattr(
 
 # Internal settings and connection check
 
-ENABLE_ZENDESK = True if ZENDESK_LOGIN and ZENDESK_PASSWORD else False
+ENABLE_ZENDESK = True if ZENDESK_ORGANIZATION and ZENDESK_LOGIN else False
 
 if ENABLE_ZENDESK:
     status_filter = ""
     for status in ZENDESK_TICKET_STATUSES_FILTER:
         status_filter += "+status%3A{status}".format(status=status)
     ZENDESK_API_URL = (
-        "https://spicesoft.zendesk.com/api/v2/search.json?query=type%3Aticket"
-        "{status_filter}".format(status_filter=status_filter)
+        "https://{org}.zendesk.com/api/v2/search.json?query=type%3Aticket"
+        "{status_filter}".format(org=ZENDESK_ORGANIZATION,
+                                 status_filter=status_filter)
     )
     resp = requests.get(ZENDESK_API_URL,
                         auth=(ZENDESK_LOGIN, ZENDESK_PASSWORD))
@@ -30,6 +32,6 @@ if ENABLE_ZENDESK:
         raise Exception(
             "Unable to retrive Zendesk data (status_code={status_code}). "
             "Please check the following settings : "
-            "ZENDESK_LOGIN, ZENDESK_PASSWORD and eventually "
-            "ZENDESK_TICKET_STATUSES_FILTER".format(
+            "ZENDESK_ORGANIZATION, ZENDESK_LOGIN, ZENDESK_PASSWORD and "
+            "eventually ZENDESK_TICKET_STATUSES_FILTER".format(
                 status_code=resp.status_code))
